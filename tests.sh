3c902daa99adf2e2c401079289f02abb359d2806
gaston-quispe <gastonquispe@gmail.com>
2017-08-29 13:12:41 -0300

    Correcciones en ficheros de pruebas

---
+++ tests.sh
@@ -0,0 +1,11 @@
+./a.out -i Tests/Entrada1.txt | diff Tests/SalidaEsperada1.txt -
+./a.out -i Tests/Entrada2.txt | diff Tests/SalidaEsperada2.txt -
+./a.out -i Tests/Entrada3.txt | diff Tests/SalidaEsperada3.txt -
+./a.out -i Tests/Entrada4.txt | diff Tests/SalidaEsperada4.txt -
+./a.out -i Tests/Entrada5.txt | diff Tests/SalidaEsperada5.txt -
+./a.out -i Tests/Entrada6.txt | diff Tests/SalidaEsperada6.txt -
+./a.out -i Tests/Entrada7.txt | diff Tests/SalidaEsperada7.txt -
+./a.out -i Tests/Entrada8.txt | diff Tests/SalidaEsperada8.txt -
+./a.out -i Tests/Entrada9.txt | diff Tests/SalidaEsperada9.txt -
+./a.out -i Tests/Entrada10.txt | diff Tests/SalidaEsperada10.txt -
+
